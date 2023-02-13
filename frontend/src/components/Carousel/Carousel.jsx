3fc7a8b1304c241198a54c91e97585de6d6dc6ce
Sachin <kumaraditya7125@gmail.com>
2023-02-13 16:47:57 +0530

    adding pagination

--- frontend/src/components/Carousel/Carousel.jsx
+++ frontend/src/components/Carousel/Carousel.jsx
@@ -6,7 +6,7 @@ import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 const Carousel = () => {
   return (
     <div style={{ backgroundColor: 'aliceblue'}}>
-    <div id="carouselExampleIndicators" className="carousel slide">
+    {/* <div id="carouselExampleIndicators" className="carousel slide">
             <div className="carousel-indicators">
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -61,7 +61,48 @@ const Carousel = () => {
             <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
                 <img src='https://cdn-icons-png.flaticon.com/128/181/181669.png' className='nextS'/>
             </button>
+            </div> */}
+            <div id="carouselExampleCaptions" class="carousel slide">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img src="https://cdn.pixabay.com/photo/2017/03/13/17/26/ecommerce-2140603__340.jpg" class="d-block w-100" alt="..."/>
+            <div class="carousel-caption d-md-block">
+                {/* <h5>First slide label</h5> */}
+                <p>Some representative placeholder content for the first slide.</p>
+                <Button variant='contained' style={{backgroundColor:'black',fontWeight:'bold',fontFamily:"'candara',sans-serif"}} className='butn'>Buy Now</Button>
             </div>
+            </div>
+            <div class="carousel-item">
+            <img src="https://mobirise.com/extensions/commercem4/assets/images/gallery01.jpg" class="d-block w-100" alt="..."/>
+            <div class="carousel-caption d-md-block">
+                {/* <h5>Second slide label</h5> */}
+                <p>Some representative placeholder content for the second slide.</p>
+                <Button variant='contained' style={{backgroundColor:'black',fontWeight:'bold',fontFamily:"'candara',sans-serif"}} className='butn'>Buy Now</Button>
+            </div>
+            </div>
+            <div class="carousel-item">
+            <img src="https://img.freepik.com/premium-photo/sale-symbol-podium-present-you-sale-shopping-concept-sale-event-shopping-sale-event-3d-illustration-background_7672-788.jpg?size=626&ext=jpg" class="d-block w-100" alt="..."/>
+            <div class="carousel-caption d-md-block">
+                {/* <h5>Third slide label</h5> */}
+                <p>Some representative placeholder content for the third slide.</p>
+                <Button variant='contained' style={{backgroundColor:'black',fontWeight:'bold',fontFamily:"'candara',sans-serif"}} className='butn'>Buy Now</Button>
+            </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+        </div>
         </div>
   )
 }
